771220b194667a07be8782cb58e8865d610dd993
Kotexat <107388102+Kotexat@users.noreply.github.com>
2022-07-14 23:11:13 +0400

    sectionSecond

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let headerDivImg = document.querySelector('#headerDivImg')
 let signUp = document.querySelector('.signUp')
 
-headerDivImg.addEventListener('click' , ()=>{
+headerDivImg.addEventListener('click', () => {
     signUp.classList.toggle('toggle')
 })
 
@@ -21,12 +21,45 @@ imgForSearch.addEventListener("click", function () {
     }
 })
 
-// let liForHeader = document.querySelectorAll('.liForHeader')
 
-// liForHeader.addEventListener('mouseover', ()=>{
-//     liForHeader.style.color = 'red'
-// })
-// liForHeader.addEventListener('mouseout', () => {
-//     liForHeader.style.color = 'black'
-// })
 
+let slideShow = document.querySelector('.slideShow')
+let slideImages = document.querySelectorAll('.slideShow img')
+let prevBtn = document.querySelector('#prevBtn')
+let nextBtn = document.querySelector('#nextBtn')
+
+
+
+
+let counter = 1
+let size = slideImages[0].clientWidth;
+
+slideShow.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+nextBtn.addEventListener('click', () => {
+    console.log('test');
+    if (counter >= slideImages.length - 1) return;
+    slideShow.style.transition = 'transform 0.4s ease-in-out';
+    counter++;
+    slideShow.style.transform = 'translateX(' + (-size * counter) + 'px)';
+})
+
+prevBtn.addEventListener('click', () => {
+    if (counter <= 0) return;
+    slideShow.style.transition = 'transform 0.4s ease-in-out';
+    counter--;
+    slideShow.style.transform = 'translateX(' + (-size * counter) + 'px)';
+})
+
+slideShow.addEventListener('transitionend', () => {
+    if (slideImages[counter].id === 'lastClone') {
+        slideShow.style.transition = 'none'
+        counter = slideImages.length - 2;
+        slideShow.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+    if (slideImages[counter].id === 'firstClone') {
+        slideShow.style.transition = 'none'
+        counter = slideImages.length - counter;
+        slideShow.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+})
